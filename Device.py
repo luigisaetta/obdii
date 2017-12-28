79ca6a403ebc38c02294ad29e1ba78bfa6c1bdf2
luigisaetta <luigi.saetta@gmail.com>
2017-12-28 10:56:47 +0100

    first code upload

---
+++ Device.py
@@ -0,0 +1,95 @@
+"""
+# This module implement a class representing a Device
+#
+# Class providing methods to interact with MQTT broker
+# L.S. 2017
+#
+"""
+# pylint: disable=invalid-name
+
+import time
+import configparser
+import paho.mqtt.client as mqtt
+
+
+#
+# Configuration for MQTT protocol
+# is written in gateway.ini file !
+# host is the broker, in my Ravello env
+
+config = configparser.ConfigParser()
+config.read('gateway.ini')
+
+HOST = config['DEFAULT']['host']
+PORT = int(config['DEFAULT']['port'])
+TIMEOUT = int(config['DEFAULT']['timeout'])
+CLIENT = config['DEFAULT']['client']
+MYQOS = int(config['DEFAULT']['myQos'])
+mqttLogging = config['DEFAULT']['mqttLogging']
+
+class Device(object):
+    """ This class encapsulate Device communication with MQTT broker """
+
+    # Constructor
+    def __init__(self):
+        self.connOK = False
+
+        # Create MQTT client and set MQTT client ID
+        self.mqttClient = mqtt.Client(CLIENT, protocol=mqtt.MQTTv311)
+        # note that the client id must be unique on the broker
+
+        # MQTT callbacks registration
+        self.mqttClient.on_message = self.on_message
+        self.mqttClient.on_connect = self.on_connect
+        self.mqttClient.on_publish = self.on_publish
+
+        if mqttLogging == "YES":
+            self.mqttClient.on_log = self.on_log
+
+    # MQTT callbacks definition
+
+    def on_connect(self, mqttc, obj, flags, connResult):
+
+        if connResult == 0:
+            self.connOK = True
+
+        print("")
+        print("MQTT Connection:...: ", self.connOK)
+        print("")
+
+    def on_message(self, mqttc, obj, msg):
+        print(msg.topic + " " + str(msg.payload))
+
+    def on_publish(self, mqttc, obj, mid):
+        # print("mid: " + str(mid))
+        pass
+
+    def on_log(self, client, userdata, level, buf):
+        print("log: ", buf)
+
+    # end MQTT callbacks definition
+
+    #
+    # Public Methods
+    #
+    def connect(self):
+        self.mqttClient.connect(HOST, PORT, TIMEOUT)
+
+        # start a background thread to process networks events. It should also
+        # handle automatical reconnection...
+        self.mqttClient.loop_start()
+
+    def wait_for_conn_ok(self):
+        while self.connOK != True:
+            print("Waiting for MQTT connection...")
+            time.sleep(1)
+
+    def publish(self, topic, msg):
+        if mqttLogging == "YES":
+            print("message published ", msg)
+
+        (result, mid) = self.mqttClient.publish(topic, msg, qos=MYQOS)
+
+    def subscribe(self, topic):
+        # for now not implemented
+        pass
---
+++ conncarmain.py
@@ -0,0 +1,88 @@
+#
+# Author: L. Saetta
+# 17 december 2017
+#
+# pylint: disable=invalid-name
+
+import json
+import time
+import datetime
+import configparser
+from Device import Device
+
+# Configuration
+#  to format datetime
+STFORMAT1 = "%Y-%m-%d %H:%M:%S"
+STFORMAT2 = "%Y-%m-%d"
+
+FNAME = "msgs" + datetime.datetime.now().strftime(STFORMAT2) + ".txt"
+
+# send a msg every 10 sec.
+sleepTime = 10
+#MQTT topic
+TOPIC_NAME = 'cardata'
+
+
+# read config from ini file
+config = configparser.ConfigParser()
+config.read('gateway.ini')
+msgLogging = config['DEFAULT']['msgLogging']
+
+def setFields():
+    # for now simulate
+    msg['carid'] = '0001'
+    msg['dtime'] = datetime.datetime.now().strftime(STFORMAT1)
+    msg['rpm'] = 1200
+    msg['speed'] = 35
+    msg['relposacc'] = 10
+    msg['tcoolant'] = 33
+    msg['toutdoor'] = 12
+    
+    msgJson = json.dumps(msg)
+    
+    return msgJson
+
+#
+# Main 
+#
+
+# MQTT connectivity is encapsulated in the Device class
+# see Device.py
+gateway = Device()
+
+gateway.connect()
+
+nMsgs = 0
+
+# main loop
+
+if msgLogging == "YES":
+    pFile = open(FNAME, "w")
+
+# wait for MQTT connection OK
+# (at this point should be connected)
+gateway.wait_for_conn_ok()
+
+print('Mqtt connected OK!')
+
+msg = {}
+
+while True:
+    nMsgs = nMsgs + 1
+    print('Sending msg ', nMsgs)
+
+    # create the msg to send from data
+    msgJson = setFields()
+
+    gateway.publish(TOPIC_NAME, msgJson)
+    
+    if msgLogging == "YES":
+        # log msg on file...
+        pFile.write(msgJson)
+        pFile.write('\n')
+
+    time.sleep(sleepTime)
+
+
+if msgLogging == "YES":
+    pFile.close()
